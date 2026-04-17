2fee1e1e5b705e99020b1a510e01faa41a32ddcb
= <=>
2026-04-17 17:40:25 +1000

    Day 6, Backend, Frontend, Docker Setup

---
+++ momentum/backend/src/main/java/com/momentum/app/MomentumApplication.java
@@ -0,0 +1,12 @@
+package com.momentum.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomentumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MomentumApplication.class, args);
+    }
+}
